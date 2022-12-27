caccbefd87a0277928372c094ef42006eca51271
Timkador <safenraider2000@gmail.com>
2022-12-27 21:21:13 +0700

    27 декабря, последнее занятие 22 года

---
+++ itogivt/script.js
@@ -0,0 +1,10 @@
+$("#flipbook").turn({
+  gradients: true,
+  acceleration: true,
+  autoCenter: true,
+});
+// $("#flipbook").turn({
+//   width: 400,
+//   height: 300,
+//   autoCenter: true,
+// });
